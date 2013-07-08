7fe6fc23db9b8e6f33566120003760a4cf488bca
Jim Frode Hoff <jimfrodehoff@gmail.com>
2013-07-08 23:55:44 +0200

    add proper pages

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -28,7 +27,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/about', routes.about);
+app.get('/contact', routes.contact);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 
 /*
- * GET home page.
+ * GET Home page.
  */
 
 exports.index = function(req, res){
   res.render('index', { title: 'HoffIT' });
+};
+
+
+/*
+ * GET About page.
+ */
+
+exports.about = function(req, res){
+  res.render('about', { title: 'HoffIT' });
+};
+
+/*
+ * GET Contact page.
+ */
+
+exports.contact = function(req, res){
+  res.render('contact', { title: 'HoffIT' });
 };
